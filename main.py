e947087b0240a3e7a0e859c65062a4f00b44fe6e
Lea Chhean <lea.chhean@edu.esiee.fr>
2025-01-01 20:03:02 +0000

    exercice sur la lecture de données

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#### Imports et définition des variables globales
+## importation de modules
 import random
-
+#fichier à lire
 FILENAME = "listes.csv"
 
 #### Fonctions secondaires
@@ -14,39 +14,60 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
+    with open(filename,'r',encoding='utf-8') as f:
+        l = []
+        for line in f:
+            lines = line.strip().split(";")
+            l2 = []
+            for elt in lines:
+                l2.append(int(elt))
+            l.append(l2)
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """retourne k-ème élément de la liste"""
+    return data[k]
 
 def get_first(l):
-    return None
+    """Retourne le premier élément"""
+    return l[0]
 
 def get_last(l):
-    return None
+    """Retourne le dernier élément"""
+    return l[len(l)-1]
 
 def get_max(l):
-    return None
+    """Retourne le maximum de la liste"""
+    max = l[0]
+    for elt in l:
+        if elt>max:
+            max = elt
+    return max
 
 def get_min(l):
-    return None
+    """Retourne le minimum de la liste"""
+    min = l[0]
+    for elt in l:
+        if elt<min:
+            min = elt
+    return min
 
 def get_sum(l):
-    return None
-
+    """Retourne la somme des éléments"""
+    resultat = 0
+    for elt in l :
+        resultat += elt
+    return resultat
 
 #### Fonction principale
 
-
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+   """Fonction princpale testant les fonctions"""
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+        k = 37
+        print(k, get_list_k(data, 37))
 
 
 if __name__ == "__main__":
